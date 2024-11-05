1e89f185fbc697c84d6cd5e6cd431917db74fb19
MissKluck <sigrid.b.kluck@gmail.com>
2024-11-05 11:25:01 +0100

    added meetingplanner and meeting classes + setup

---
+++ Meeting.cs
@@ -0,0 +1,19 @@
+using System.Reflection.Metadata.Ecma335;
+
+public class Meeting
+{
+    /*
+    * tidspunkt datetime/iso8601
+    * deltakere liste<string>
+    * legg til deltakere
+    */
+
+    public string? Name { get; set; }
+    public int Date { get; set; }
+
+    public DateTime Date(get; set; )
+
+    List<string> participants;
+
+
+}
---
+++ MeetingPlanner.cs
@@ -0,0 +1,11 @@
+public class MeetingPlanner
+{
+    /*
+    * List<meetings> meetings
+    * lag nytt møte (meeting newMeeting)
+    * lese av møter()
+    */
+    List<Meeting> meetings;
+
+    Meeting = newMeeting
+}
